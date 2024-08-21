387dc48fc8f2f6331edf9b2507bd84908d23152b
sallam <108616378+h0tak88r@users.noreply.github.com>
2024-08-21 09:37:37 +0300

    Update main.go

--- cmd/subfalcon/main.go
+++ cmd/subfalcon/main.go
@@ -17,15 +17,17 @@ func main() {
 	var domainList string
 	var webhook string
 	var monitor bool
+	var singleDomain string // New flag for a single domain
 
 	// Parse flags
 	flag.StringVar(&domainList, "l", "", "Specify a file containing a list of domains")
 	flag.StringVar(&webhook, "wh", "", "Specify the Discord webhook URL")
 	flag.BoolVar(&monitor, "m", false, "Enable subdomain monitoring")
+	flag.StringVar(&singleDomain, "d", "", "Specify a single domain for processing") // New flag
 	flag.Parse()
 
 	// Set the configuration values
-	config.SetConfig(domainList, webhook, monitor)
+	config.SetConfig(domainList, webhook, monitor, singleDomain)
 
 	// Validate flags
 	if err := config.ValidateFlags(); err != nil {
